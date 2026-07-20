cd5f14a33a8a3bfa636b83b5d0be4eb0deb32432
Aaron Aslan <aaronaslan8@gmail.com>
2026-07-19 22:03:47 -0400

    Added clarifying line to comment

--- Health_Insurance_App/Plans/Plan Recommendation/Views/SpecificDetailsView.swift
+++ Health_Insurance_App/Plans/Plan Recommendation/Views/SpecificDetailsView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SpecificDetailsView: View {
     var body: some View {
-        // TODO: once you have the dataset, create this page. It should be a list of all of the specific details about the selected plan.
+        // TODO: once you have the dataset/set variables in the specific variales category, create this page. It should be a list of all of the specific details about the selected plan.
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
