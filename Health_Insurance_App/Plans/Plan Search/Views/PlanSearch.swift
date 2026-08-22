0ada63d9668205881e676eb5cb9e32009ad500fd
Aaron Aslan <aaronaslan8@gmail.com>
2026-08-22 11:47:21 -0400

    Added questionaire to PlanSearch

--- Health_Insurance_App/Plans/Plan Search/Views/PlanSearch.swift
+++ Health_Insurance_App/Plans/Plan Search/Views/PlanSearch.swift
@@ -1,38 +1,37 @@
 //
-//  PlanSearch.swift
-//  Health_Insurance_App
+// PlanSearch.swift
+// Health_Insurance_App
 //
-//  Created by Aaron Aslan on 4/19/26.
+// Created by Aaron Aslan on 4/19/26.
 //
 
 import SwiftUI
+// TODO: think of a different name besides Plan Search
 
 struct PlanSearch: View {
-    @StateObject var vm = PlansViewModel()
+    @StateObject var vm = QuestionsViewModel()
     @State private var selection: Int = 0
+    @State var questions: Questions
     
     var body: some View {
         Background {
             ScrollView {
                 VStack {
-                    ForEach(Array(vm.planCardModels.enumerated()), id: \.element.id) { index, plan in
-                        NavigationLink {
-                            PlanDetailsView(selection: index)
-                        } label: {
-                            PlanCard(plan: plan)
-                        }
-                        .tint(.primary)
+                    QuestionCard(
+                        question: vm.currentQuestion.question,
+                        answers: vm.currentQuestion.answers
+                    ) { answer in
+                        vm.answerSelected(answer)
                     }
                 }
                 .padding(.horizontal)
-            }
-            .navigationTitle("Plan Search")
+            } .navigationTitle("Plan Search")
         }
     }
 }
 
 #Preview {
     NavigationStack {
-        PlanSearch()
+        PlanSearch(questions: Questions())
     }
 }
