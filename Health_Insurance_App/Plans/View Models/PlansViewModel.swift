24394d286dfed7dcfb86f530e12c59b03cc7b34b
Aaron Aslan <aaronaslan8@gmail.com>
2026-09-23 18:11:05 -0400

    Add function to plans view model to sort plans in plan search

--- Health_Insurance_App/Plans/View Models/PlansViewModel.swift
+++ Health_Insurance_App/Plans/View Models/PlansViewModel.swift
@@ -13,7 +13,10 @@ internal import Combine
 class PlansViewModel: ObservableObject {
     private let engine = PlanFilteringEngine()
     
+    @Published private(set) var filteredPlans: [PlanVars] = []
     @Published private(set) var recommendedPlans: [RecommendationPlanVars] = []
+    
+    @Published var currentSort: SortingFactors = .premium
 
     // output the variables for each recommended plan card
     var recommendationCardModels: [RecommendationCardVars] {
@@ -30,13 +33,31 @@ class PlansViewModel: ObservableObject {
     
     // output the variables for each filtered plan card
     var planCardModels: [PlanCardVars] {
-        engine.filteredPlans.map { plan in
+        filteredPlans.map { plan in
             PlanCardVars(
                 id: plan.id,
                 planName: plan.planName,
                 deductible: plan.deductible,
                 premium: plan.premium
             )
+        }
+    }
+    
+    func refresh(scores: ScoreDictionary) {
+        filteredPlans = engine.filterPlans(scores: scores)
+        sortPlans(by: .premium)
+    }
+    
+    // sort plans for plan search page
+    func sortPlans(by sortingFactor: SortingFactors) {
+        currentSort = sortingFactor
+        switch sortingFactor {
+        case .premium:
+            filteredPlans.sort { $0.premium < $1.premium }
+        case .deductible:
+            filteredPlans.sort { $0.deductible < $1.deductible }
+        case .provider:
+            filteredPlans.sort { $0.coName < $1.coName }
         }
     }
 
