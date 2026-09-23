d85fee7344be76df79602df2400b5ca07c8684da
Aaron Aslan <aaronaslan8@gmail.com>
2026-09-23 18:08:07 -0400

    Add option to retake the questionnaire and popup to sort plans in plan search

--- Health_Insurance_App/Plans/Plan Search/Views/PlanSearchView.swift
+++ Health_Insurance_App/Plans/Plan Search/Views/PlanSearchView.swift
@@ -9,30 +9,89 @@ import SwiftUI
 // TODO: think of a different name besides Plan Search
 // TODO: animate from question to question
 struct PlanSearchView: View {
-    @StateObject var vm = PlansViewModel()
+    @StateObject var plansVM = PlansViewModel()
+    @StateObject var questionsVM = QuestionsViewModel()
+    var engine = PlanFilteringEngine()
+    
     @State private var selection: Int = 0
     @State var questions: Questions
+    
     @State private var showSheet = true
-    var engine = PlanFilteringEngine()
+    @State private var showPopup = false
     
     var body: some View {
         Background {
             ScrollView {
-                VStack {
-                    ForEach(Array(vm.planCardModels.enumerated()), id: \.element.id) { index, plan in
-                        NavigationLink() {
-                            PlanDetailsView(selection: index)
+                ZStack {
+                    VStack {
+                        Button {
+                            showSheet = true
+                            questionsVM.questionIndex = 0
                         } label: {
-                            PlanCard(plan: plan)
+                            HStack {
+                                Text("Retake Questionnaire")
+                                    .padding()
+                                    .foregroundStyle(.white)
+                            }
+                            .frame(maxWidth: .infinity)
+                            .background(
+                                RoundedRectangle(cornerRadius: 16)
+                                    .fill(.tint)
+                            )
                         }
-                        .tint(.primary)
+                        .buttonStyle(.plain)
+                        
+                        ForEach(Array(plansVM.planCardModels.enumerated()), id: \.element.id) { index, plan in
+                            NavigationLink() {
+                                PlanDetailsView(selection: index, recommended: false)
+                            } label: {
+                                PlanCard(plan: plan)
+                            }
+                            .tint(.primary)
+                        }
                     }
-                }
-                .padding(.horizontal)
-                .sheet(isPresented: $showSheet, onDismiss: {
-                    engine.updateQualifyingVars()
-                }) {
-                    QuestionnaireSheetView()
+                    .padding(.horizontal)
+                    .sheet(isPresented: $showSheet, onDismiss: {
+                        plansVM.refresh(scores: engine.updateScores(selectedAnswers: questionsVM.selectedAnswers))
+                    }) {
+                        QuestionnaireSheetView(vm: questionsVM)
+                    }
+                    .toolbar {
+                        ToolbarItem(placement: .topBarTrailing) {
+                            Button {
+                                withAnimation(.spring(response: 0.25, dampingFraction: 0.85)) {
+                                    showPopup.toggle()
+                                }
+                            } label: {
+                                Image(systemName: "slider.horizontal.3")
+                            }
+                        }
+                    }
+                    if showPopup {
+                        // TODO: make the color.clear expand to the edges of the screen
+                        ZStack {
+                            Color.clear
+                                .contentShape(Rectangle())
+                                .ignoresSafeArea()
+                                .onTapGesture {
+                                    withAnimation(.easeInOut) {
+                                        showPopup = false
+                                    }
+                                }
+                            
+                            VStack {
+                                HStack {
+                                    Spacer()
+                                    SortPlansPopup(vm: plansVM) {
+                                        showPopup = false
+                                    }
+                                    .padding(.top, 10)
+                                    .padding(.trailing, 10)
+                                }
+                                Spacer()
+                            }
+                        }
+                    }
                 }
             }
             .navigationTitle("Plan Search")
